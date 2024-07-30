637e5cb259c6ed41a4d111fba0880abdb3d3ffea
louohr <louise.ohrner@gmail.com>
2024-07-30 11:12:00 +0200

    add solutions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Exercise 1
 
 // 1
-async function fetchPokemon() {
+async function fetchPokemons() {
   try {
     let response = await fetch("https://santosnr6.github.io/Data/pokemons.json");
     let data = await response.json();
@@ -11,10 +11,10 @@ async function fetchPokemon() {
   }
 }
 
-fetchPokemon();
+fetchPokemons();
 
 // 2
-async function fetchPokemon() {
+async function fetchPokemons() {
   try {
     let response = await fetch("https://santosnr6.github.io/Data/pokemons.json");
     let data = await response.json();
@@ -24,10 +24,10 @@ async function fetchPokemon() {
   }
 }
 
-fetchPokemon();
+fetchPokemons();
 
 // 3
-async function fetchPokemon() {
+async function fetchPokemons() {
   fetch("https://santosnr6.github.io/Data/pokemons.json")
     .then((response) => response.json())
     .then((data) => {
@@ -40,3 +40,100 @@ async function fetchPokemon() {
 
 // Exercise 2
 // 1
+async function fetchDogs() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/dogs.json");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchDogs();
+
+// 2
+async function fetchDogs() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/dogs.json");
+    let data = await response.json();
+    data.forEach((dog) => console.log(dog));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchDogs();
+
+// 3
+async function fetchDogs() {
+  fetch("https://majazocom.github.io/Data/dogs.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((dog) => {
+        document.body.innerHTML += `${dog.name} <br>`;
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+// Exercise 3
+// 1
+async function fetchBooks() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/books.json");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchBooks();
+
+// 2
+async function fetchBooks() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/books.json");
+    let data = await response.json();
+    data
+      .filter((book) => book.pages < 500)
+      .forEach((book) => {
+        document.body.innerHTML += `${book.title} - ${book.pages} pages <br>`;
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Exercise 4
+// 1
+async function fetchAttendees() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/attendees.json");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchAttendees();
+
+// 2
+async function fetchAttendees() {
+  try {
+    let response = await fetch("https://majazocom.github.io/Data/attendees.json");
+    let data = await response.json();
+    document.body.innerHTML = "";
+    data
+      .filter((attendee) => attendee.attending === true)
+      .forEach((attendee) => {
+        document.body.innerHTML += `${attendee.name} <br>`;
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchAttendees();
